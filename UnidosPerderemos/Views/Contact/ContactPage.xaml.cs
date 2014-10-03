584b9c6eed45640dc4bd50167d2d7d4b80223f9c
Marlon Santos Constante <marlon.constante@gmail.com>
2014-10-03 16:33:11 -0300

    Adicionando xamarin.mobile

---
+++ UnidosPerderemos/Views/Contact/ContactPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace UnidosPerderemos
+{
+	public partial class ContactPage : ContentPage
+	{
+		public ContactPage()
+		{
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// Builds the list view.
+		/// </summary>
+		/// <returns>The list view.</returns>
+		ListView BuildListView()
+		{
+			var listView = new ListView {
+				ItemTemplate = new DataTemplate(typeof(EnterpriseSelectionItemCell)),
+				ItemsSource = DependencyService.Get<IAddressBook>().FindAllContacts()
+			};
+			return listView;
+		}
+	}
+}
