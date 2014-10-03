b994a8798105e7195beb7e96878d58c7a67f00b1
Marlon Santos Constante <marlon.constante@gmail.com>
2014-10-03 16:34:15 -0300

    Adicionando página de contato

---
+++ UnidosPerderemos/Views/Contact/ContactPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace UnidosPerderemos
+{
+	public partial class ContactPage : ContentPage
+	{
+		public ContactPage()
+		{
+			InitializeComponent();
+
+			ConfigListView();
+			LoadContacts();
+
+			Content = ListView;
+		}
+
+		/// <summary>
+		/// Configs the list view.
+		/// </summary>
+		void ConfigListView()
+		{
+			ListView = new ListView {
+				ItemTemplate = new DataTemplate(typeof(ContactCell))
+			};
+		}
+
+		/// <summary>
+		/// Loads the contacts.
+		/// </summary>
+		async void LoadContacts()
+		{
+			ListView.ItemsSource = await DependencyService.Get<IAddressBookService>().FindAllContacts();
+		}
+
+		/// <summary>
+		/// Gets or sets the list view.
+		/// </summary>
+		/// <value>The list view.</value>
+		public ListView ListView {
+			get;
+			set;
+		}
+	}
+}
