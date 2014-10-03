7c46cfecf58e57df7a2b408703b81956253d02ad
Marlon Santos Constante <marlon.constante@gmail.com>
2014-10-03 13:30:36 -0300

    Atualizando git ignore

--- Android/Resources/Resource.designer.cs
+++ Android/Resources/Resource.designer.cs
@@ -24,6 +24,10 @@ namespace UnidosPerderemos.Android
 			global::Android.Runtime.ResourceIdManager.UpdateIdValues();
 		}
 		
+		public static void UpdateIdValues()
+		{
+		}
+		
 		public partial class Attribute
 		{
 			
