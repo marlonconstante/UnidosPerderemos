f2a889f43be3369d82de202dd951f6e1da920ea3
Marlon Santos Constante <marlon.constante@gmail.com>
2014-10-06 15:58:26 -0300

    Adicionando página de metas

---
+++ UnidosPerderemos/Views/Goal/GoalPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace UnidosPerderemos
+{
+	public partial class GoalPage : ContentPage
+	{
+		public GoalPage()
+		{
+			InitializeComponent();
+
+			Content = new Image {
+				Aspect = Aspect.AspectFill,
+				Source = ImageSource.FromFile("BackgroundGoal.png")
+			};
+		}
+	}
+}
