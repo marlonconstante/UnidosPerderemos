aebcec8785b68228ec059ff76bcc2bad75478313
Marlon Santos Constante <marlon.constante@gmail.com>
2014-10-13 13:35:37 -0300

    Adicionando página de histórico

---
+++ UnidosPerderemos/Views/History/HistoryPage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace UnidosPerderemos
+{
+	public class HistoryPage : ContentPage, IControlPage
+	{
+		public HistoryPage()
+		{
+			SetUp();
+		}
+
+		/// <summary>
+		/// Sets up.
+		/// </summary>
+		void SetUp()
+		{
+			Title = "Histórico";
+			Icon = ImageSource.FromFile("History.png") as FileImageSource;
+			BackgroundImage = "BackgroundGoal.png";
+		}
+
+		/// <summary>
+		/// Preferreds the status bar style.
+		/// </summary>
+		/// <returns>The status bar style.</returns>
+		public StatusBarStyle PreferredStatusBarStyle()
+		{
+			return StatusBarStyle.Dark;
+		}
+
+		/// <summary>
+		/// Determines whether this instance is show navigation bar.
+		/// </summary>
+		/// <returns><c>true</c> if this instance is show navigation bar; otherwise, <c>false</c>.</returns>
+		public bool IsShowNavigationBar()
+		{
+			return true;
+		}
+
+		/// <summary>
+		/// Determines whether this instance is show status bar.
+		/// </summary>
+		/// <returns><c>true</c> if this instance is show status bar; otherwise, <c>false</c>.</returns>
+		public bool IsShowStatusBar()
+		{
+			return true;
+		}
+	}
+}
