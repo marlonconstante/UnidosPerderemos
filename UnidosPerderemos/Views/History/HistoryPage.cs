4c84b4bc9ee5ae5574e15c217c04a8d8e132b8a4
Marlon Santos Constante <marlon.constante@gmail.com>
2014-10-13 12:12:48 -0300

    Adicionando página de histórico

---
+++ UnidosPerderemos/Views/History/HistoryPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace UnidosPerderemos
+{
+	public class HistoryPage : ContentPage
+	{
+		public HistoryPage()
+		{
+			SetUp();
+		}
+
+		/// <summary>
+		/// Sets up.
+		/// </summary>
+		void SetUp()
+		{
+			Title = "Histórico";
+			Icon = ImageSource.FromFile("History.png") as FileImageSource;
+			BackgroundImage = "BackgroundGoal.png";
+		}
+	}
+}
