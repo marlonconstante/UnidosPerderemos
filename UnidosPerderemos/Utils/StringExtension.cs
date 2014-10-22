0fcfd110358b4d72af70f9c3b3b93e5dab6c35dc
Marlon Santos Constante <marlon.constante@gmail.com>
2014-10-22 12:13:20 -0200

    Adicionando extensões para String

---
+++ UnidosPerderemos/Utils/StringExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace UnidosPerderemos.Utils
+{
+	public static class StringExtension
+	{
+		/// <summary>
+		/// Tos the first uppercase.
+		/// </summary>
+		/// <returns>The first uppercase.</returns>
+		/// <param name="text">Text.</param>
+		public static string ToFirstUppercase(this string text)
+		{
+			if (string.IsNullOrEmpty(text))
+			{
+				return string.Empty;
+			}
+			return char.ToUpper(text[0]) + text.Substring(1);
+		}
+
+		/// <summary>
+		/// Tos the first lowercase.
+		/// </summary>
+		/// <returns>The first lowercase.</returns>
+		/// <param name="text">Text.</param>
+		public static string ToFirstLowercase(this string text)
+		{
+			if (string.IsNullOrEmpty(text))
+			{
+				return string.Empty;
+			}
+			return char.ToLower(text[0]) + text.Substring(1);
+		}
+	}
+}
