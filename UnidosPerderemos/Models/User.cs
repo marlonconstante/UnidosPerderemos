51e148fb58e133a03b82a1a0d53b3dd217f0cc0e
Marlon Santos Constante <marlon.constante@gmail.com>
2014-10-22 12:29:27 -0200

    Adicionando serviço de Login (Parse)

---
+++ UnidosPerderemos/Models/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace UnidosPerderemos.Models
+{
+	public class User
+	{
+		public User()
+		{
+		}
+
+		/// <summary>
+		/// Gets or sets the name.
+		/// </summary>
+		/// <value>The name.</value>
+		public string Name {
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets the email.
+		/// </summary>
+		/// <value>The email.</value>
+		public string Email {
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets the username.
+		/// </summary>
+		/// <value>The username.</value>
+		public string Username {
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets the password.
+		/// </summary>
+		/// <value>The password.</value>
+		public string Password {
+			get;
+			set;
+		}
+	}
+}
---
+++ UnidosPerderemos/Services/IUserService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnidosPerderemos.Models;
+
+namespace UnidosPerderemos.Services
+{
+	public interface IUserService
+	{
+		/// <summary>
+		/// Login the specified user.
+		/// </summary>
+		/// <param name="user">User.</param>
+		Task<bool> Login(User user);
+
+		/// <summary>
+		/// Signs up.
+		/// </summary>
+		/// <returns>The up.</returns>
+		/// <param name="user">User.</param>
+		Task<bool> SignUp(User user);
+
+		/// <summary>
+		/// Gets the current user.
+		/// </summary>
+		/// <value>The current user.</value>
+		User CurrentUser {
+			get;
+		}
+	}
+}
