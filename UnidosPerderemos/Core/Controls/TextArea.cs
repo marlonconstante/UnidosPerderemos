24ad0e152def0d39f829ff422095f03f84d826df
Marlon Santos Constante <marlon.constante@gmail.com>
2014-11-05 18:22:44 -0200

    Adicionando componente TextArea

---
+++ UnidosPerderemos/Core/Controls/TextArea.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace UnidosPerderemos.Core.Controls
+{
+	public class TextArea : Editor
+	{
+		/// <summary>
+		/// The font property.
+		/// </summary>
+		public static readonly BindableProperty FontProperty = BindableProperty.Create<TextArea, Font>(p => p.Font, Font.Default);
+
+		/// <summary>
+		/// The text color property.
+		/// </summary>
+		public static readonly BindableProperty TextColorProperty = BindableProperty.Create<TextArea, Color>(p => p.TextColor, Color.Default);
+
+		public TextArea()
+		{
+			SetUp();
+		}
+
+		/// <summary>
+		/// Sets up.
+		/// </summary>
+		void SetUp()
+		{
+			Font = Font.OfSize("Roboto-Light", 23);
+			TextColor = Color.FromHex("fcfcfa");
+			BackgroundColor = Color.White.MultiplyAlpha(0.1d);
+			HeightRequest = 127d;
+		}
+
+		/// <summary>
+		/// Requests the focus.
+		/// </summary>
+		public void RequestFocus()
+		{
+			Device.BeginInvokeOnMainThread(() => {
+				Focus();
+			});
+		}
+
+		/// <summary>
+		/// Requests the unfocus.
+		/// </summary>
+		public void RequestUnfocus()
+		{
+			Device.BeginInvokeOnMainThread(() => {
+				Unfocus();
+			});
+		}
+
+		/// <summary>
+		/// Gets or sets the font.
+		/// </summary>
+		/// <value>The font.</value>
+		public Font Font {
+			get {
+				return (Font) GetValue(FontProperty);
+			}
+			set {
+				SetValue(FontProperty, value);
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the color of the text.
+		/// </summary>
+		/// <value>The color of the text.</value>
+		public Color TextColor {
+			get {
+				return (Color) GetValue(TextColorProperty);
+			}
+			set {
+				SetValue(TextColorProperty, value);
+			}
+		}
+	}
+}
---
+++ iOS/Renderers/Controls/TextAreaRenderer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Xamarin.Forms.Platform.iOS;
+using Xamarin.Forms;
+using UnidosPerderemos;
+using MonoTouch.UIKit;
+using UnidosPerderemos.Core.Controls;
+using MonoTouch.CoreAnimation;
+using System.Drawing;
+
+[assembly: ExportRenderer(typeof(TextArea), typeof(UnidosPerderemos.iOS.Renderers.Controls.TextAreaRenderer))]
+namespace UnidosPerderemos.iOS.Renderers.Controls
+{
+	public class TextAreaRenderer : EditorRenderer
+	{
+		public TextAreaRenderer()
+		{
+		}
+
+		/// <Docs>Lays out subviews.</Docs>
+		/// <summary>
+		/// Layouts the subviews.
+		/// </summary>
+		public override void LayoutSubviews()
+		{
+			base.LayoutSubviews();
+
+			SetUp();
+		}
+
+		/// <summary>
+		/// Sets up.
+		/// </summary>
+		void SetUp()
+		{
+			if (!Initialized)
+			{
+				Target.Font = Source.Font.ToUIFont();
+				Target.TextColor = Source.TextColor.ToUIColor();
+				Target.TextContainerInset = new UIEdgeInsets(10f, 10f, 10f, 10f);
+
+				Initialized = true;
+			}
+		}
+
+		/// <summary>
+		/// Gets the source.
+		/// </summary>
+		/// <value>The source.</value>
+		TextArea Source {
+			get {
+				return Element as TextArea;
+			}
+		}
+
+		/// <summary>
+		/// Gets the target.
+		/// </summary>
+		/// <value>The target.</value>
+		UITextView Target {
+			get {
+				return Control;
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets a value indicating whether this
+		/// <see cref="UnidosPerderemos.iOS.Renderers.Controls.TextAreaRenderer"/> is initialized.
+		/// </summary>
+		/// <value><c>true</c> if initialized; otherwise, <c>false</c>.</value>
+		bool Initialized {
+			get;
+			set;
+		} = false;
+	}
+}
