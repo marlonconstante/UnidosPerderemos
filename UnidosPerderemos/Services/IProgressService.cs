36cb5fd0f58152aaffc96b82906dcb216b6df5c0
Marlon Santos Constante <marlon.constante@gmail.com>
2014-11-11 11:20:22 -0200

    Adicionando serviço de progresso

---
+++ UnidosPerderemos/Services/IProgressService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnidosPerderemos.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace UnidosPerderemos.Services
+{
+	public interface IProgressService
+	{
+		/// <summary>
+		/// Loads the daily.
+		/// </summary>
+		/// <returns>The daily.</returns>
+		Task<DailyProgress> LoadDaily();
+
+		/// <summary>
+		/// Finds all.
+		/// </summary>
+		/// <returns>The all.</returns>
+		/// <param name="user">User.</param>
+		Task<IList<IProgress>> FindAll(User user);
+
+		/// <summary>
+		/// Save the specified progress.
+		/// </summary>
+		/// <param name="progress">Progress.</param>
+		Task<bool> Save(IProgress progress);
+	}
+}
