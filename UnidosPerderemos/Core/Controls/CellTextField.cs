822ade571bc53e12c95b5492469f81482e33b81e
Marlon Santos Constante <marlon.constante@gmail.com>
2014-11-12 16:17:05 -0200

    Adicionando documentação

--- UnidosPerderemos/Core/Controls/CellTextField.cs
+++ UnidosPerderemos/Core/Controls/CellTextField.cs
@@ -5,6 +5,11 @@ namespace UnidosPerderemos.Core.Controls
 {
 	public class CellTextField : TextField
 	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="UnidosPerderemos.Core.Controls.CellTextField"/> class.
+		/// </summary>
+		/// <param name="text">Text.</param>
+		/// <param name="keyboard">Keyboard.</param>
 		public CellTextField(string text, Keyboard keyboard)
 		{
 			Text = text;
