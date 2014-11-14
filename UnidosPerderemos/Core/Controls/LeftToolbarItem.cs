22ada2ab64848578037dd405604fd1305dce0f04
Marlon Santos Constante <marlon.constante@gmail.com>
2014-11-14 08:59:37 -0200

    Adicionando novos componentes: LeftToolbarItem e RightToolbarItem

---
+++ UnidosPerderemos/Core/Controls/LeftToolbarItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace UnidosPerderemos.Core.Controls
+{
+	/// <summary>
+	/// Left toolbar item.
+	/// </summary>
+	public class LeftToolbarItem : ToolbarItem
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="UnidosPerderemos.Core.Controls.LeftToolbarItem"/> class.
+		/// </summary>
+		public LeftToolbarItem()
+		{
+		}
+	}
+}
---
+++ UnidosPerderemos/Core/Controls/RightToolbarItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace UnidosPerderemos.Core.Controls
+{
+	/// <summary>
+	/// Right toolbar item.
+	/// </summary>
+	public class RightToolbarItem : ToolbarItem
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="UnidosPerderemos.Core.Controls.RightToolbarItem"/> class.
+		/// </summary>
+		public RightToolbarItem()
+		{
+		}
+	}
+}
