a24abb9698356804fda43f707bde6ebd9d48f3ce
Marlon Santos Constante <marlon.constante@gmail.com>
2014-11-24 17:38:36 -0200

    Adicionando classe DateTimeExtension

---
+++ UnidosPerderemos/Utils/DateTimeExtension.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace UnidosPerderemos.Utils
+{
+	public static class DateTimeExtension
+	{
+		/// <summary>
+		/// The first day of week.
+		/// </summary>
+		const DayOfWeek FirstDayOfWeek = DayOfWeek.Monday;
+
+		/// <summary>
+		/// Weeks the of year.
+		/// </summary>
+		/// <returns>The of year.</returns>
+		/// <param name="date">Date.</param>
+		public static int WeekOfYear(this DateTime date)
+		{
+			var calendar = CultureInfo.CurrentCulture.Calendar;
+			return calendar.GetWeekOfYear(date, CalendarWeekRule.FirstFourDayWeek, FirstDayOfWeek);
+		}
+
+		/// <summary>
+		/// Starts the of week.
+		/// </summary>
+		/// <returns>The of week.</returns>
+		/// <param name="date">Date.</param>
+		public static DateTime StartOfWeek(this DateTime date)
+		{
+			var days = date.DayOfWeek - FirstDayOfWeek;
+			if (days < 0)
+			{
+				days += 7;
+			}
+			return date.AddDays(-1 * days).Date;
+		}
+	}
+}
