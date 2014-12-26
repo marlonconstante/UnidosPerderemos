ed6824d45ec9fc1c88de10b3b69632cd700f00fa
Marlon Santos Constante <marlon.constante@gmail.com>
2014-12-26 11:26:19 -0200

    Adicionando extensão para converter string para double

--- UnidosPerderemos/Utils/StringExtension.cs
+++ UnidosPerderemos/Utils/StringExtension.cs
@@ -31,5 +31,20 @@ namespace UnidosPerderemos.Utils
 			}
 			return char.ToLower(text[0]) + text.Substring(1);
 		}
+
+		/// <summary>
+		/// Parses the double.
+		/// </summary>
+		/// <returns>The double.</returns>
+		/// <param name="text">Text.</param>
+		public static double ParseDouble(this string text)
+		{
+			try {
+				return double.Parse(text);
+			} catch (Exception ex)
+			{
+				return 0d;
+			}
+		}
 	}
 }
